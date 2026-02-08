f85eaf5ae12a4e025c77fe2d7e49b7cf118d5d71
believelody <believelody@gmail.com>
2026-02-09 00:08:14 +0100

    [episode_5]: Cloudflare worker location headers

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -2,6 +2,12 @@ import { Hono } from "hono";
 
 export const App = new Hono<{ Bindings: Env }>();
 
-App.get("/:id", (c) => {
-    return c.json({ message: "Hello World!" });
+App.get("/:id", (ctx) => {
+	if (!ctx.req.raw.cf) {
+		throw new Error('CF object is not available in the request context.');
+	}
+
+	const { country, latitude, longitude } = ctx.req.raw.cf;
+
+	return ctx.json({ country, latitude, longitude });
 });
